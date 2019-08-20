e984d93772e8f6c7fe12e2445cdc3a3e042716ea
Di0102 <Di0102@classroom.net>
2019-08-20 13:56:10 -0300

    Gordão

--- backend/src/main/java/br/com/impacta/model/Cliente.java
+++ backend/src/main/java/br/com/impacta/model/Cliente.java
@@ -2,6 +2,6 @@ package br.com.impacta.model;
 
 public class Cliente {
 	public static void main(String[] args) {
-		System.out.println("ola mundo"); 
+		System.out.println("Olá MUndão Doido"); 
 	}
 }
