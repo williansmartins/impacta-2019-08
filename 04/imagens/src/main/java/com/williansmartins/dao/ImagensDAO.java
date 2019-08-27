3e97ed0ecde3c840535a7c2d8fafbf932c62a151
Di0104 <Di0104@classroom.net>
2019-08-27 09:56:35 -0300

    criação da pasta dao e adição do DAO de imagens

---
+++ 04/imagens/src/main/java/com/williansmartins/dao/ImagensDAO.java
@@ -0,0 +1,21 @@
+package com.williansmartins.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ImagensDAO {
+	public static void main(String[] args) {
+
+		try {
+			String url = "jdbc:mysql://localhost/sakila?useTimezone=true&serverTimezone=UTC";
+			String user = "root";
+			String password = "Imp@ct@";
+			
+			Connection con = DriverManager.getConnection(url,user,password);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
