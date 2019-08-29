cc3702828f1b01afd1e3b112bc11552b7e7f6c04
Di0114 <Di0114@classroom.net>
2019-08-29 13:23:41 -0300

    Conexão com banco de dados

---
+++ 14/backend/src/main/java/com/williansmartins/imagens/model/Imagem.java
@@ -0,0 +1,48 @@
+package com.williansmartins.imagens.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Imagem {
+	@Id @GeneratedValue
+	private Long id;
+	private String url;
+
+	public Imagem() {}
+	
+	
+	public Imagem(Long id, String url) {
+		super();
+		this.id = id;
+		this.url = url;
+	}
+
+
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Imagem [id=" + id + ", url=" + url + "]";
+	}
+
+	
+}
--- 14/imagens/src/main/java/com/williansmartins/imagens/dao/ImagensDAO.java
+++ 14/imagens/src/main/java/com/williansmartins/imagens/dao/ImagensDAO.java
@@ -1,19 +1,59 @@
 package com.williansmartins.imagens.dao;
 
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.williansmartins.imagens.model.Customer;
 
 public class ImagensDAO {
-	public static void main(String[] args) {
+	//Criação atributos
+	private static Connection con;
+	private static ResultSet resultSet;
+	private static Statement statement;
+	static List<Customer> customers = new ArrayList<Customer>();
+
+	public List<Customer> buscarCustomers() {
 		try {
-			String url = "jdbc:mysql://localhost/sakila";
+			//Conexão
+			String url = "jdbc:mysql://localhost/sakila?useTimezone=tru&serverTimezone=UTC";
 			String user = "root";
 			String password = "Imp@ct@";
+
+			System.out.println("Conectando...");
+			con = DriverManager.getConnection(url, user, password);
+			System.out.println("Conectado com Sucesso!");
+
+			//Selecionando todos da tabela
+			statement = con.createStatement();
+			String sql = "select * from customer";
+			resultSet = statement.executeQuery(sql);
 			
-			Connection con = DriverManager.getConnection(url, user, password);
+			
+			//Preenchendo a lista com os atributos e a lista com a lista + imprimindo
+			while (resultSet.next()) {
+				
+				Customer customer = new Customer();
+				customer.setId(resultSet.getInt("customer_id"));
+				customer.setNome(resultSet.getString("first_name"));
+				customer.setSobrenome(resultSet.getString("last_name"));
+				customer.setEmail(resultSet.getString("email"));
+
+				customers.add(customer);
+				System.out.println(customer);
+			}
+	
 		} catch (SQLException e) {
 			e.printStackTrace();
+			
 		}
+		
+		return customers;
 	}
+	
+	
 }
