e3d051bae43fbd3e1f0c46cc482b5f7d56fa5a74
Willians Martins <professorwillians@gmail.com>
2019-08-29 13:18:46 -0300

    add controller do album - gacp2

--- projeto-final/backend/src/main/java/com/williansmartins/imagens/model/Imagem.java
+++ projeto-final/backend/src/main/java/com/williansmartins/imagens/model/Imagem.java
@@ -1,8 +1,23 @@
 package com.williansmartins.imagens.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Imagem {
 	
+	@Id @GeneratedValue
+	private Long id;
 	private String url;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getUrl() {
 		return url;
@@ -14,7 +29,7 @@ public class Imagem {
 
 	@Override
 	public String toString() {
-		return "Imagem [url=" + url + "]";
+		return "Imagem [id=" + id + ", url=" + url + "]";
 	}
 
 }
---
+++ projeto-final/backend/src/main/java/com/williansmartins/imagens/repository/ImagensRepository.java
@@ -0,0 +1,9 @@
+package com.williansmartins.imagens.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.williansmartins.imagens.model.Imagem;
+
+public interface ImagensRepository extends CrudRepository<Imagem, Long> {
+
+}
