f9d149f299ceb3e32d4be16ca14415e6610ed6a8
Di0101 <Di0101@classroom.net>
2019-09-03 10:53:47 -0300

    atualização

---
+++ 01/backend professor/src/main/java/com/williansmartins/imagens/model/Imagem.java
@@ -0,0 +1,31 @@
+package com.williansmartins.imagens.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Imagem {
+	
+	@Id @GeneratedValue
+	private Long id;
+	private String url;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	@Override
+	public String toString() {
+		return "Imagem [id=" + id + ", url=" + url + "]";
+	}
+
+	
+}
