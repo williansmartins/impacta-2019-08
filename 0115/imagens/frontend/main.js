17977f9e75a82ef62aa5158676bafdad76b3bd65
Di0115 <Di0115@classroom.net>
2019-09-04 14:17:39 -0300

    ATUALIZACAO5

--- 0115/imagens/frontend/main.js
+++ 0115/imagens/frontend/main.js
@@ -1,1 +1,24 @@
-alert(456);
+var app = angular.module('app', []);
+app.controller('AlbunsController', function($scope) {
+    $scope.albuns = [
+        {
+            "nome" : "valor1"
+        },
+        {
+            "nome" : "valor2"
+        },
+        {
+            "nome" : "valor2"
+        },
+        {
+            "nome" : "valor2"
+        },
+        {
+            "nome" : "valor2"
+        },
+        {
+            "nome" : "valor3"
+        }
+    ];
+
+});
