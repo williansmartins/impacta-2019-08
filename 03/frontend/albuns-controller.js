67fe06d9ebe57f6456a7d334841d4d57fad34441
Di0103 <Di0103@classroom.net>
2019-09-05 10:13:32 -0300

    Atualizando controller e style

--- 03/frontend/albuns-controller.js
+++ 03/frontend/albuns-controller.js
@@ -1,15 +1,7 @@
 app.controller('AlbunsController', function($scope, $http) {
     $scope.albuns=new Object(); 
       
-    $http({
-        method: 'GET',
-        url: 'http://localhost:8080/albuns'
-    }).then(function successCallback(response) {
-        console.info("deu bom");
-        $scope.albuns = response.data;
-    }, function errorCallback(response) {
-        console.info("deu ruim");
-    });
+    
 
     $scope.remover = function(id){
         $http({
@@ -17,11 +9,28 @@ app.controller('AlbunsController', function($scope, $http) {
             url: 'http://localhost:8080/albuns/' + id
         }).then(function successCallback(response) {
             console.info("deu bom");
+            $("#album_" + id).hide();
+        }, function errorCallback(data, status, headers, config, statusText , xhrStatus ) {
+            console.info("deu ruim");
+        });
+    }
+
+    var buscarAlbuns = function(){
+        $http({
+            method: 'GET',
+            url: 'http://localhost:8080/albuns'
+        }).then(function successCallback(response) {
+            console.info("deu bom");
             $scope.albuns = response.data;
         }, function errorCallback(response) {
             console.info("deu ruim");
         });
     }
-   
+
+    var init = function(){
+        buscarAlbuns();
+    }
+
+    init();
 
 });
