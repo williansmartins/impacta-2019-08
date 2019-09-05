07ab85d70e6e3c7d4fa866c89666b33c65d4c22e
Di0111 <Di0111@classroom.net>
2019-09-05 10:50:18 -0300

    att

--- 11/Projetos/Projeto Imagem/backend/src/main/java/com/williansmartins/imagens/repository/ImagensRepository.java
+++
@@ -1,9 +0,0 @@
-package com.williansmartins.imagens.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.williansmartins.imagens.model.Imagem;
-
-public interface ImagensRepository extends CrudRepository<Imagem, Long> {
-
-}
