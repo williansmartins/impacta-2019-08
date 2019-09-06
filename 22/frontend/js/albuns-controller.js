78936cc41e497bcfbb63a1b46b2e5e5e5c9a52cf
Di0122@classroom.net <Di0122@classroom.net>
2019-09-06 09:13:47 -0300

    atualizando o modal

--- 22/frontend/js/albuns-controller.js
+++ 22/frontend/js/albuns-controller.js
@@ -1,24 +1,24 @@
-app.controller('AlbunsController', function($scope, $http) {
-    $scope.albuns=new Object(); 
-      
-    
+app.controller('AlbunsController', function ($scope, $http) {
+    $scope.albuns = new Object();
 
-    $scope.remover = function(id){
+
+
+    $scope.remover = function (id) {
         $http({
             method: 'DELETE',
-            url: 'http://localhost:8080/albuns/' + id
+            url: 'http://172.16.2.7:8080/albuns/' + id
         }).then(function successCallback(response) {
             console.info("deu bom");
             $("#album_" + id).hide();
-        }, function errorCallback(data, status, headers, config, statusText , xhrStatus ) {
+        }, function errorCallback(data, status, headers, config, statusText, xhrStatus) {
             console.info("deu ruim");
         });
     }
 
-    var buscarAlbuns = function(){
+    var buscarAlbuns = function () {
         $http({
             method: 'GET',
-            url: 'http://localhost:8080/albuns'
+            url: 'http://172.16.2.7:8080/albuns'
         }).then(function successCallback(response) {
             $scope.albuns = response.data;
         }, function errorCallback(response) {
@@ -26,7 +26,44 @@ app.controller('AlbunsController', function($scope, $http) {
         });
     }
 
-    var init = function(){
+    $scope.salvarAlbum = function (){
+        if ($scope.idAlbum == null) {
+            $http({
+                method: 'POST',
+                url: 'http://172.16.2.7:8080/albuns',
+                data: { 'nome': $scope.nomeDoAlbum }
+            }).then(function successCallback(response) {
+                alert("sucesso!!"),
+                    buscarAlbuns(),
+                    $scope.nome = "";
+            }, function errorCallback(data, status, headers, config, statusText, xhrStatus) {
+                console.info("deu ruim");
+            });
+        }
+
+        else {
+            $http({
+                method: 'PUT',
+                url: 'http://172.16.2.7:8080/albuns/' + $scope.idAlbum,
+                data: { 'nome': $scope.nomeDoAlbum }
+            }).then(function successCallback(response) {
+                alert("sucesso!!"),
+                    buscarAlbuns(),
+                    $scope.nome = "";
+            }, function errorCallback(data, status, headers, config, statusText, xhrStatus) {
+                console.info("deu ruim");
+            });
+        }
+
+    }
+
+    $scope.editar = function (album) {
+        console.info(album);
+        $scope.nomeDoAlbum = album.nome;
+        $scope.idAlbum = album.id;
+    }
+
+    var init = function () {
         buscarAlbuns();
     }
 
