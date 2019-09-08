6eeb021533fb3d849c1db69b5f6e9032b2e7e6b1
Willians MAC <professorwillians@gmail.com>
2019-09-08 17:49:10 -0300

    organizando inserts

---
+++ projeto-final/backend/src/main/resources/albuns.sql
@@ -0,0 +1,2 @@
+insert into album (nome) values ('album1');	
+insert into album (nome) values ('album2');
---
+++ projeto-final/backend/src/main/resources/relacionamentos.sql
@@ -0,0 +1,9 @@
+insert into album_imagens (album_id, imagens_id) values (1, 1);
+insert into album_imagens (album_id, imagens_id) values (1, 2);
+insert into album_imagens (album_id, imagens_id) values (1, 3);
+insert into album_imagens (album_id, imagens_id) values (1, 4);
+
+insert into album_imagens (album_id, imagens_id) values (2, 5);
+insert into album_imagens (album_id, imagens_id) values (2, 6);
+insert into album_imagens (album_id, imagens_id) values (2, 7);
+insert into album_imagens (album_id, imagens_id) values (2, 8);	
