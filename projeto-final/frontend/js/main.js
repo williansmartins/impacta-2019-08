eccbecfc11c4a387392dd577a35597d24f20745c
Di0116 <Di0116@classroom.net>
2019-09-09 09:43:45 -0300

    mudando album style

--- projeto-final/frontend/js/main.js
+++ projeto-final/frontend/js/main.js
@@ -1,4 +1,4 @@
 var app = angular.module('app', ['ngRoute']);
 var host = "http://172.16.2.7:8080";
-var host = "http://localhost:8080";
+/*var host = "http://localhost:8080";*/
 //var host = "https://2886795277-8080-ollie07.environments.katacoda.com";
